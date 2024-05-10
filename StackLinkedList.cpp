848d0b40aabf94d55def9f2cfe55f3179d4610a2
salmaarayyan <salmaarayyan@gmail.com>
2024-05-10 15:47:14 +0700

    menambah method push didalam class stack

--- StackLinkedList.cpp
+++ StackLinkedList.cpp
@@ -21,4 +21,14 @@ public:
     Stack() {
         top = NULL;  //initialize the stack with a null top pointer
     }
+
+//push operation : insert an element onto the top of the stack
+int push(int value) {
+    Node* newNode = new Node();  //1. allocate memory for the new node
+    newNode->data = value;  //2. assign value
+    newNode->next = top;  //3. set the next pointer of the new node to the current top node
+    top = newNode;  //4. update the top pointer to the new node
+    cout << "Push value : " << value << endl;
+    return value;
+}
 };
