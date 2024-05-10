ee27b61f3604bce8227368cbb9f1882d42fc5d94
salmaarayyan <salmaarayyan@gmail.com>
2024-05-10 15:41:45 +0700

    membuat initialisasi stack dengan top = NULL

--- StackLinkedList.cpp
+++ StackLinkedList.cpp
@@ -16,4 +16,9 @@ public:
 class Stack {
 private:
     Node* top;  //pointer to the top node of the stack
-}
+
+public:
+    Stack() {
+        top = NULL;  //initialize the stack with a null top pointer
+    }
+};
