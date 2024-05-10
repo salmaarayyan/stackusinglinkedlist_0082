131a9b9d8aa2508a0aec11ff33ac7fd4b2289326
salmaarayyan <salmaarayyan@gmail.com>
2024-05-10 16:08:08 +0700

    menampilkan tulisan "push, pop, peek, exit dan masukan pilihanmu"

--- StackLinkedList.cpp
+++ StackLinkedList.cpp
@@ -68,7 +68,12 @@ int main() {
     int choice = 0;
     int value;
 
-    while (choice) {
+    while (choice != 5) {
+        cout << "1. Push\n";
+        cout << "2. Pop\n";
+        cout << "3. Peek\n";
+        cout << "4. Exit\n";
+        cout << "Enter your choice : ";
         
     }
 }
